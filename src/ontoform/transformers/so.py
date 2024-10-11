2dfda4a3c3d26c0aa5cc2c52358a588da7c1b086
ricardo <remo87@gmail.com>
2024-10-11 13:27:14 +0100

    actualizar schema primera version de node

--- src/ontoform/transformers/so.py
+++ src/ontoform/transformers/so.py
@@ -7,27 +7,27 @@ schema = pl.Schema({
           "lbl": pl.String(),
           "meta": pl.Struct({
             "basicPropertyValues": 
-              pl.list(pl.Struct({
+              pl.List(pl.Struct({
                 "pred": pl.String(),
                 "val": pl.String()
               }))
             ,
-            "comments": pl.list(pl.String()),
+            "comments": pl.List(pl.String()),
             "definition": pl.Struct({
               "val": pl.String(),
-              "xrefs": pl.list(pl.String())
+              "xrefs": pl.List(pl.String())
             }),
             "deprecated": pl.Boolean(),
-            "subsets": pl.list(pl.String()),
+            "subsets": pl.List(pl.String()),
             "synonyms": 
-              pl.list(pl.Struct({
+              pl.List(pl.Struct({
                 "pred": pl.String(),
                 "synonymType": pl.String(),
                 "val": pl.String(),
-                "xrefs": pl.list(pl.String())
+                "xrefs": pl.List(pl.String())
               })),
             "xrefs": 
-              pl.list(pl.Struct({
+              pl.List(pl.Struct({
                 "val": pl.String()
               }))
           }),
@@ -49,7 +49,7 @@ def transform(src: Path, dst: Path) -> None:
     
     # prepare property data
     property_list = pl.DataFrame(initial['graphs'][0][0]['nodes']).filter(
-        pl.col('type') != 'PROPERTY',
+        (pl.col('type') == 'PROPERTY') & pl.col('meta').is_null() & pl.col('id').str.contains('formats').not_(),
     )
     
     property_values = property_list.with_columns(
@@ -62,7 +62,68 @@ def transform(src: Path, dst: Path) -> None:
         strict=False,
         schema=schema,
     ).filter(
-        pl.col('type') != 'CLASS',
+        pl.col('type') == 'CLASS',
     )
+    
+    # cleanup and renaming
+    cleaned_node_list = (
+        node_list.filter(
+            pl.col('type') == 'CLASS'
+        )
+        .unnest('meta')
+        .with_columns(
+            id_at='obo:'+pl.col('id').str.split('/').list.last(),
+            id=''+pl.col('id').str.split('/').list.last().str.replace('_', ':'),
+            definition=pl.col('definition').struct['val'],
+            dbXRefs=pl.col('xrefs').list.eval(pl.element().struct.field('val').unique()),
+            type='owl:'+pl.col('type').str.to_lowercase().str.replace('c', 'C'),
+        )
+        .drop(
+            'basicPropertyValues',
+            'comments',
+            'subsets',
+            'xrefs',
+        )
+        .rename({'lbl': 'label', 'id_at': '@id'})
+    )
+    
+    # fix synonyms
+    synonym_columns = ['hasExactSynonym', 'hasRelatedSynonym', 'hasNarrowSynonym', 'hasBroadSynonym']
+    synonyms_list = (
+        (
+            cleaned_node_list['id', 'synonyms']
+            .explode('synonyms')
+            .filter(pl.col('synonyms').struct['pred'].is_in(synonym_columns))
+            .unnest('synonyms')
+            .with_columns(val=pl.col('val').str.replace_all('\n', '').str.strip_chars())
+            .group_by(['id', 'pred'])
+            .agg(pl.col('val').unique())
+            .pivot(values='val', index='id', columns='pred', aggregate_function='first')
+        )
+        .with_columns([pl.col(col).fill_null([]) for col in synonym_columns])
+        .with_columns(
+            synonyms=pl.struct(
+                hasExactSynonym=pl.col('hasExactSynonym'),
+                hasRelatedSynonym=pl.col('hasRelatedSynonym'),
+                hasNarrowSynonym=pl.col('hasNarrowSynonym'),
+                hasBroadSynonym=pl.col('hasBroadSynonym'),
+            )
+        )
+        .select(['id', 'synonyms'])
+    )
+    nodes_with_synonyms = cleaned_node_list.drop('synonyms').join(synonyms_list, on='id', how='left')
 
-    pass
+    # compute obsolete related entities
+    # properties = (
+    #     node_list
+    #     .unnest('meta')
+    #     .explode('basicPropertyValues')
+    #     .unnest('basicPropertyValues')
+    #     .select(['id', 'val', 'pred'])
+    #     .with_columns('pred', pl.col('pred'))
+    # )
+    
+    # merge the properties with the nodes
+    
+    # write the result
+    nodes_with_synonyms.write_ndjson(dst)
