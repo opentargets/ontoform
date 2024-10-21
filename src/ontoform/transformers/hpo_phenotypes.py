6a7184e7ad2727ed4a2432b5bee5d793fd7b448c
Javier Ferrer <javier.f.g@um.es>
2024-10-21 17:20:00 +0100

    Add hpo_phenotypes transformation

---
+++ src/ontoform/transformers/hpo_phenotypes.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+import polars as pl
+
+
+def transform(src: Path, dst: Path) -> None:
+    # load the ontology
+    initial = pl.read_csv(src, separator='\t', comment_prefix='#')
+
+    n1 = pl.DataFrame().with_columns(
+        databaseId=initial['database_id'],
+        diseaseName=initial['disease_name'],
+        qualifier=initial['qualifier'],
+        HPOId=initial['hpo_id'],
+        references=initial['reference'].str.split(';').list.unique(),
+        evidenceType=initial['evidence'],
+        onset=initial['onset'].str.replace_all('HP:', 'HP_').str.split(';').list.unique(),
+        frequency=initial['frequency'].str.replace_all('HP:', 'HP_'),
+        sex=initial['sex'],
+        modifiers=initial['modifier'].str.replace_all('HP:', 'HP_').str.split(';').list.unique(),
+        aspect=initial['aspect'],
+        biocuration=initial['biocuration'].str.strip_chars_end(),
+        resource=pl.lit('HPO'),
+    )
+
+    # write the result
+    n1.write_ndjson(
+        dst,
+    )
