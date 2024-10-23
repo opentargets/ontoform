c9f66a410ecc5246d8767119cf7d0289fcc15be7
remo87 <remo87@gmail.com>
2024-10-23 14:01:32 +0100

    3499 Sequence Ontology Transformation (#1)

--- src/ontoform/transformers/so.py
+++ src/ontoform/transformers/so.py
@@ -2,8 +2,51 @@ from pathlib import Path
 
 import polars as pl
 
-schema = pl.Schema({'id': pl.String()})
-
+schema = pl.Schema({     
+          "id": pl.String(),
+          "lbl": pl.String(),
+          "meta": pl.Struct({
+            "basicPropertyValues": 
+              pl.List(pl.Struct({
+                "pred": pl.String(),
+                "val": pl.String()
+              }))
+            ,
+            "comments": pl.List(pl.String()),
+            "definition": pl.Struct({
+              "val": pl.String(),
+              "xrefs": pl.List(pl.String())
+            }),
+            "deprecated": pl.Boolean(),
+            "subsets": pl.List(pl.String()),
+            "synonyms": 
+              pl.List(pl.Struct({
+                "pred": pl.String(),
+                "synonymType": pl.String(),
+                "val": pl.String(),
+                "xrefs": pl.List(pl.String())
+              })),
+            "xrefs": 
+              pl.List(pl.Struct({
+                "val": pl.String()
+              }))
+          }),
+          "type": pl.String()
+        })
 
 def transform(src: Path, dst: Path) -> None:
-    pass
+    # Reads the so owl file
+    initial = pl.read_json(src)
+    
+    
+    # prepare node data
+    node_list = pl.DataFrame(
+        initial['graphs'][0][0]['nodes'],
+        strict=False,
+        schema=schema,
+    ).filter(
+        pl.col('type') == 'CLASS',
+    )
+    
+    # write the result
+    node_list.write_ndjson(dst)
