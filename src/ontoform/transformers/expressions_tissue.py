946742678ceef0507210bb3ec4e94fdf51b3b8cc
ricardo <remo87@gmail.com>
2024-10-25 09:36:32 +0100

    Update code comments

--- src/ontoform/transformers/expressions_tissue.py
+++ src/ontoform/transformers/expressions_tissue.py
@@ -7,18 +7,21 @@ def transform(src: Path, dst: Path) -> None:
     # load the tissue expressions
     initial = pl.read_json(src).unnest('tissues')
     
-    # Unnest the tissues and use the key as a column
+    # Unnest every value of the tissues structure and use the key as the column tissue_id
     tissue_list = []
     for key, value in initial.to_dict().items():
         
-        
+        # Create a DataFrame with the value and unnest it
         df = pl.DataFrame(value).unnest(key)
         
+        # Add tissue_id column with having the key as the value
         df = df.with_columns(pl.lit(key).alias('tissue_id'))
         
+        # Append the DataFrame to the list
         tissue_list.append(df)
         
     # The list needs to be concatenated because it's a list of DataFrames
     output = pl.concat(tissue_list)
     
+    # Write the output to the destination
     output.write_ndjson(dst)
