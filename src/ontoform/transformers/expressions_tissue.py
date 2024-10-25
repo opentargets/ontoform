19ca93baac012861cb273f9090c1effe9198da66
ricardo <remo87@gmail.com>
2024-10-25 12:10:13 +0100

    refactor loop to get structures

--- src/ontoform/transformers/expressions_tissue.py
+++ src/ontoform/transformers/expressions_tissue.py
@@ -1,26 +1,23 @@
 from pathlib import Path
 
 import polars as pl
+import polars.selectors as cs
 
 def transform(src: Path, dst: Path) -> None:
     
     # load the tissue expressions
     initial = pl.read_json(src).unnest('tissues')
     
-    # Unnest every value of the tissues structure and use the key as the column tissue_id
-    tissue_list = []
-    for key, value in initial.to_dict().items():
-        
-        # Create a DataFrame with the value and unnest it
-        df = pl.DataFrame(value).unnest(key)
-        
-        # Add tissue_id column with having the key as the value
-        df = df.with_columns(pl.lit(key).alias('tissue_id'))
-        
-        # Append the DataFrame to the list
-        tissue_list.append(df)
-        
-    # The list needs to be concatenated because it's a list of DataFrames
+    # Get all the column names
+    columns = initial.columns
+    
+    # Create a list of dataframes from the json values and the tissue_id
+    tissue_list = [initial
+                   .select(tissue=pl.col(column))
+                   .unnest('tissue')
+                   .with_columns(tissue_id=pl.lit(column)) for column in columns]
+    
+    # Concatenate the list of dataframes
     output = pl.concat(tissue_list)
     
     # Write the output to the destination
