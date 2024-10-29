1cab3a5659a93a06ddb0071cd348d77bdb4c2d1f
ricardo <remo87@gmail.com>
2024-10-29 15:18:11 +0000

    remove null labels, add tests

--- src/ontoform/transformers/so.py
+++ src/ontoform/transformers/so.py
@@ -2,43 +2,39 @@ from pathlib import Path
 
 import polars as pl
 
-schema = pl.Schema({     
-          "id": pl.String(),
-          "lbl": pl.String(),
-          "meta": pl.Struct({
-            "basicPropertyValues": 
-              pl.List(pl.Struct({
-                "pred": pl.String(),
-                "val": pl.String()
-              }))
-            ,
-            "comments": pl.List(pl.String()),
-            "definition": pl.Struct({
-              "val": pl.String(),
-              "xrefs": pl.List(pl.String())
-            }),
-            "deprecated": pl.Boolean(),
-            "subsets": pl.List(pl.String()),
-            "synonyms": 
-              pl.List(pl.Struct({
-                "pred": pl.String(),
-                "synonymType": pl.String(),
-                "val": pl.String(),
-                "xrefs": pl.List(pl.String())
-              })),
-            "xrefs": 
-              pl.List(pl.Struct({
-                "val": pl.String()
-              }))
-          }),
-          "type": pl.String()
-        })
+schema = pl.Schema(
+    {
+        'id': pl.String(),
+        'lbl': pl.String(),
+        'meta': pl.Struct(
+            {
+                'basicPropertyValues': pl.List(pl.Struct({'pred': pl.String(), 'val': pl.String()})),
+                'comments': pl.List(pl.String()),
+                'definition': pl.Struct({'val': pl.String(), 'xrefs': pl.List(pl.String())}),
+                'deprecated': pl.Boolean(),
+                'subsets': pl.List(pl.String()),
+                'synonyms': pl.List(
+                    pl.Struct(
+                        {
+                            'pred': pl.String(),
+                            'synonymType': pl.String(),
+                            'val': pl.String(),
+                            'xrefs': pl.List(pl.String()),
+                        }
+                    )
+                ),
+                'xrefs': pl.List(pl.Struct({'val': pl.String()})),
+            }
+        ),
+        'type': pl.String(),
+    }
+)
+
 
 def transform(src: Path, dst: Path) -> None:
     # Reads the so owl file
     initial = pl.read_json(src)
-    
-    
+
     # prepare node data
     node_list = pl.DataFrame(
         initial['graphs'][0][0]['nodes'],
@@ -47,11 +43,15 @@ def transform(src: Path, dst: Path) -> None:
     ).filter(
         pl.col('type') == 'CLASS',
     )
-    
-    output = node_list.filter(pl.col('id').str.contains('SO_')).select(
-      id=pl.col('id').str.split('/').list.last().str.replace('_', ':'), 
-      label=pl.col('lbl')
+
+    # filter out non SO terms and terms without labels. Then select the id and label columns
+    output = node_list.filter(
+        pl.col('id').str.contains('SO_'),
+        pl.col('lbl').is_not_null(),
+    ).select(
+        id=pl.col('id').str.split('/').list.last().str.replace('_', ':'),
+        label=pl.col('lbl'),
     )
-    
+
     # write the result
     output.write_ndjson(dst)
---
+++ tools/test_so.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# This script is meant to be run from the root of the repository
+if [ ! -d ./examples/efo_otar_slim ]; then
+    echo "Please run this script from the root of the repository (like: ./tests/$(basename "$0"))"
+    exit 1
+fi
+
+# set up stuff
+set -x
+set -e
+cd ./examples/so || exit
+mkdir -p ./output
+
+# get so
+curl -Ls https://raw.githubusercontent.com/The-Sequence-Ontology/SO-Ontologies/refs/heads/master/Ontology_Files/so.json > ./input/so.json
+
+# run the transformation
+uv run ontoform so ./input/so.json ./output/so.jsonl
+
+# drop definitions, we already know they will be different
+# and the definition alternatives, which are not there in the ontoform output
+# also, sort rows
+jq -c '{id:.id,label:.label}' ./input/so-oldpis.json | sort > ./output/so-oldpis.jsonl
+
+# sort the object keys
+jq -s "." ./output/so-oldpis.jsonl | jq --sort-keys "." | jq -c ".[]" > ./output/so-oldpis_sort.jsonl
+jq -s "." ./output/so.jsonl | jq --sort-keys "." | jq -c ".[]" | sort > ./output/so_sort.jsonl
+
+# compare the outputs
+diff --color ./output/so-oldpis_sort.jsonl ./output/so_sort.jsonl
