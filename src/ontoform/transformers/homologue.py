1b3262cbd523a81407475b290af6b4b96075d2dc
ricardo <remo87@gmail.com>
2024-11-04 10:53:34 +0000

    Refactor homologue transformer to accept binary input and generate temporary file for processing

--- src/ontoform/transformers/homologue.py
+++ src/ontoform/transformers/homologue.py
@@ -1,26 +1,31 @@
 import json
 import subprocess
 from pathlib import Path
+from typing import BinaryIO
+from uuid import uuid4
 
 import polars as pl
 
 
-def transform(src: Path, dst: Path) -> None:
+def transform(src: BinaryIO, dst: BinaryIO) -> None:
     jq_command = '.genes | {"genes": map({id: .id, name: .name})}'
+
+    # Generate a temporary file path tp store the source file
+    temp_path = Path(f'/tmp/{uuid4()}.json')
+
+    temp_path.write_bytes(src.read())
 
     # Ejecuta jq con subprocess
     result = subprocess.run(
-        ['jq', jq_command, src],
+        ['jq', jq_command, temp_path],
         capture_output=True,
         text=True
     )
 
     # Verifica si el comando se ejecutó correctamente
     if result.returncode == 0:
-        # Carga la salida JSON como un objeto de Python
         output = json.loads(result.stdout)
 
-        # Crea un DataFrame de Polars a partir de la salida JSON
         input_genes = pl.DataFrame(output, strict=False, infer_schema_length=3)
 
         # extract genes list
@@ -29,5 +34,4 @@ def transform(src: Path, dst: Path) -> None:
         # # read id and name
         output = genes_list.select(['id', 'name'])
 
-        # # write the result
         output.write_csv(dst, separator='\t', include_header=True)
