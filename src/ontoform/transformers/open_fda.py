31cdea543e111704d2040a3d322633477957a47b
ricardo <remo87@gmail.com>
2024-11-05 12:26:25 +0000

    reformat file

--- src/ontoform/transformers/open_fda.py
+++ src/ontoform/transformers/open_fda.py
@@ -6,13 +6,8 @@ import polars as pl
 
 def transform(src: BinaryIO, dst: BinaryIO) -> None:
     with zipfile.ZipFile(src, 'r') as zip_file:
-
         for item in zip_file.filelist:
-
             with zip_file.open(item) as file:
-
                 initial = pl.read_json(file)
-
                 results = initial.select('results').explode('results').unnest('results')
-
                 results.write_parquet(dst)
