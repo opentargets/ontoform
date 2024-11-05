2424c4dd049a8e08ff037a1ca623b681fb6ca905
ricardo <remo87@gmail.com>
2024-11-05 09:04:33 +0000

    add open fda transformator

---
+++ src/ontoform/transformers/open_fda.py
@@ -0,0 +1,18 @@
+import zipfile
+from typing import BinaryIO
+
+import polars as pl
+
+
+def transform(src: BinaryIO, dst: BinaryIO) -> None:
+    with zipfile.ZipFile(src, 'r') as zip_file:
+
+        for item in zip_file.filelist:
+
+            with zip_file.open(item) as file:
+
+                initial = pl.read_json(file)
+
+                results = initial.select('results').explode('results').unnest('results')
+
+                results.write_parquet(dst)
