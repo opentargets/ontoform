fd02a278166a06cf6cd291650c710e611a411985
remo87 <remo87@gmail.com>
2024-11-12 11:52:12 +0000

    Update src/ontoform/transformers/expressions_normal_tissue.py
    Co-authored-by: Javier Ferrer <javier.f.g@um.es>

--- src/ontoform/transformers/expressions_normal_tissue.py
+++ src/ontoform/transformers/expressions_normal_tissue.py
@@ -8,8 +8,7 @@ from ontoform.util import SupportedFormats
 
 
 def transform(src: BinaryIO, dst: BinaryIO, format: SupportedFormats) -> None:
-    with zipfile.ZipFile(src, 'r') as zip_file:
-        for item in zip_file.filelist:
-            with zip_file.open(item) as file:
-                with gzip.open(dst, 'wb') as gzip_file:
-                    gzip_file.write(file.read())
+   with zipfile.ZipFile(src) as zip_file:
+        with zip_file.open("normal_tissue.tsv") as file:
+            with gzip.open(dst, "wb") as gzip_file:
+                gzip_file.write(file.read())
