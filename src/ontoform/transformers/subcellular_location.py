118cd5c41af9f1dae7299b848b701db201a54aec
ricardo <remo87@gmail.com>
2024-11-12 11:36:32 +0000

    apply pr comments

--- src/ontoform/transformers/subcellular_location.py
+++ src/ontoform/transformers/subcellular_location.py
@@ -7,8 +7,6 @@ from ontoform.util import SupportedFormats
 
 def transform(src: BinaryIO, dst: BinaryIO, format: SupportedFormats) -> None:
     with zipfile.ZipFile(src, 'r') as zip_file:
-        for item in zip_file.filelist:
-            with zip_file.open(item) as file:
-                if item.filename == 'subcellular_location.tsv':
-                    with gzip.open(dst, 'wb') as gzip_file:
-                        gzip_file.write(file.read())
+        with zip_file.open('subcellular_location.tsv') as file:
+            with gzip.open(dst, 'wb') as gzip_file:
+                gzip_file.write(file.read())
