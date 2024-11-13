ae4371de673d257e7e7b8e3b586c5db013b72973
ricardo <remo87@gmail.com>
2024-11-13 14:56:42 +0000

    add binary and genes socres and update to parquet

---
+++ src/ontoform/transformers/target_binary_scores.py
@@ -0,0 +1,10 @@
+from typing import BinaryIO
+import gzip
+import polars as pl
+
+from ontoform.util import SupportedFormats, write_dst
+
+def transform(src: BinaryIO, dst: BinaryIO, format: SupportedFormats) -> None:
+    with gzip.open(src, 'rb') as gzip_file:
+        df = pl.read_csv(gzip_file.read())
+        write_dst(df, dst, format)
